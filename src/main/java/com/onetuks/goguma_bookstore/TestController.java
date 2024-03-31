3e0ca3fd89117962d131f2012ff626f97381ec76
onetuks <onetuks@naver.com>
2024-04-01 02:01:29 +0900

    Test: Splotless 테스트2

--- src/main/java/com/onetuks/goguma_bookstore/TestController.java
+++ src/main/java/com/onetuks/goguma_bookstore/TestController.java
@@ -1,17 +1,13 @@
 package com.onetuks.goguma_bookstore;
 
 import org.springframework.web.bind.annotation.GetMapping;
-
-
-
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
-    @GetMapping(path = "/")
-    public String home() {
-        return "home";
-    }
-
+  @GetMapping(path = "/")
+  public String home() {
+    return "home";
+  }
 }
