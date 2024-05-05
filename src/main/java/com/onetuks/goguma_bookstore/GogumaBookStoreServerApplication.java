5929e8879b171ccca43210b9a021ca459f61d0ac
onetuks <onetuks@naver.com>
2024-05-06 04:53:22 +0900

    Fix: Spring Security 프로젝트 적용

--- src/main/java/com/onetuks/goguma_bookstore/GogumaBookStoreServerApplication.java
+++ src/main/java/com/onetuks/goguma_bookstore/GogumaBookStoreServerApplication.java
@@ -2,8 +2,10 @@ package com.onetuks.goguma_bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.onetuks")
 public class GogumaBookStoreServerApplication {
 
   public static void main(String[] args) {
