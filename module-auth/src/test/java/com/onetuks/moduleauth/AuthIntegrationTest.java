e6ee42917f6baefafab4cdafbc44982123a138a6
onetuks <onetuks@naver.com>
2024-05-09 13:36:43 +0900

    Fix: CI 실패 대응

--- module-auth/src/test/java/com/onetuks/moduleauth/AuthIntegrationTest.java
+++ module-auth/src/test/java/com/onetuks/moduleauth/AuthIntegrationTest.java
@@ -77,8 +77,12 @@ public class AuthIntegrationTest {
           "jdbc:mysql://" + localDbHost + ":" + localDbPort + "/goguma-bookstore");
       properties.put("spring.datasource.username", "root");
       properties.put("spring.datasource.password", "root1234!");
-      properties.put("openapi.data-go-kr.secret-key", "HJGVGr90METjyp2CXImquifQLs4eOdNEkTGf31Da6kqByBCIbkbn8FxO%2BgxrKvAaNzQx7FpjSqNvZCSzBIOY9Q%3D%3D");
-      properties.put("openapi.center-lib.secret-key", "6d8a07116c7eab073f5d98230fc96f44fb8285eaa18f2bc8481c3f85ea789a8b");
+      properties.put(
+          "openapi.data-go-kr.secret-key",
+          "HJGVGr90METjyp2CXImquifQLs4eOdNEkTGf31Da6kqByBCIbkbn8FxO%2BgxrKvAaNzQx7FpjSqNvZCSzBIOY9Q%3D%3D");
+      properties.put(
+          "openapi.center-lib.secret-key",
+          "6d8a07116c7eab073f5d98230fc96f44fb8285eaa18f2bc8481c3f85ea789a8b");
 
       var redistHost = redis.getHost();
       var redistPort = redis.getFirstMappedPort();
