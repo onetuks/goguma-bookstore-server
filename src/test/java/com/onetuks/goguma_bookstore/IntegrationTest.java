a19cafc008eb5baa0bffb750d6fa4eddb2127af0
onetuks <onetuks@naver.com>
2024-05-09 23:27:39 +0900

    Revert "Refactor: Spring Cloud Config TestContainers 내부화"
    This reverts commit bd662a2497e23452a2c09f7529637f1e589938af.

--- src/test/java/com/onetuks/goguma_bookstore/IntegrationTest.java
+++ src/test/java/com/onetuks/goguma_bookstore/IntegrationTest.java
@@ -26,7 +26,6 @@ public class IntegrationTest {
 
   private static final int LOCAL_DB_PORT = 3306;
   private static final int LOCAL_DB_MIGRATION_PORT = 0;
-  private static final int CLOUD_CONFIG_PORT = 8888;
   private static final int DURATION = 300;
   private static final String DOCKER_COMPOSE_PATH =
       System.getProperty("rootDir") + "/db/test/docker-compose.yaml";
@@ -34,10 +33,6 @@ public class IntegrationTest {
   static {
     containers =
         new ComposeContainer(new File(DOCKER_COMPOSE_PATH))
-            .withExposedService(
-                "cloud-config",
-                CLOUD_CONFIG_PORT,
-                Wait.forListeningPort().withStartupTimeout(Duration.ofSeconds(DURATION)))
             .withExposedService(
                 "local-db",
                 LOCAL_DB_PORT,
@@ -58,13 +53,9 @@ public class IntegrationTest {
     public void initialize(@NotNull ConfigurableApplicationContext applicationContext) {
       Map<String, String> properties = new HashMap<>();
 
-      var cloudConfigHost = containers.getServiceHost("cloud-config", CLOUD_CONFIG_PORT);
-      var cloudConfigPort = containers.getServicePort("cloud-config", CLOUD_CONFIG_PORT);
       var localDbHost = containers.getServiceHost("local-db", LOCAL_DB_PORT);
       var localDbPort = containers.getServicePort("local-db", LOCAL_DB_PORT);
 
-      properties.put(
-          "spring.cloud.config.uri", "http://" + cloudConfigHost + ":" + cloudConfigPort);
       properties.put(
           "spring.datasource.url",
           "jdbc:mysql://" + localDbHost + ":" + localDbPort + "/goguma-bookstore-test");
