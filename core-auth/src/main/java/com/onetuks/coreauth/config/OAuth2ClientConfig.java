0cff385d7764d651c33c616a899d1c2ac3c00928
onetuks <onetuks@naver.com>
2024-05-21 02:28:47 +0900

    Refactor: WebClientConfig 빈 중복 제거

--- core-auth/src/main/java/com/onetuks/coreauth/config/OAuth2ClientConfig.java
+++ core-auth/src/main/java/com/onetuks/coreauth/config/OAuth2ClientConfig.java
@@ -1,5 +1,6 @@
 package com.onetuks.coreauth.config;
 
+import com.onetuks.coreweb.config.WebClientConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/onetuks/goguma_bookstore/GogumaBookStoreServerApplication.java
+++ src/main/java/com/onetuks/goguma_bookstore/GogumaBookStoreServerApplication.java
@@ -2,11 +2,15 @@ package com.onetuks.goguma_bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.onetuks")
 public class GogumaBookStoreServerApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(GogumaBookStoreServerApplication.class, args);
   }
 }
+
+
